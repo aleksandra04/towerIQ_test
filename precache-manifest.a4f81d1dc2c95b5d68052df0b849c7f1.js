5b8ae60533139e8660ff750e491cf103c38e86bd
Alexandra Boyko <Alexboyko04@gmail.com>
2020-01-10 17:33:44 +0200

    Updates

---
+++ precache-manifest.a4f81d1dc2c95b5d68052df0b849c7f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e9bc4c3617b78009f87b185f070aba9",
+    "url": "/towerIQ_test/index.html"
+  },
+  {
+    "revision": "c6abb3b8b0e17fe34001",
+    "url": "/towerIQ_test/static/css/main.c4dd1723.chunk.css"
+  },
+  {
+    "revision": "72abaf1e999a055f4993",
+    "url": "/towerIQ_test/static/js/2.0338dcda.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/towerIQ_test/static/js/2.0338dcda.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c6abb3b8b0e17fe34001",
+    "url": "/towerIQ_test/static/js/main.b10bff59.chunk.js"
+  },
+  {
+    "revision": "cb05e28514a35edfedd4",
+    "url": "/towerIQ_test/static/js/runtime-main.245ec87c.js"
+  }
+]);
